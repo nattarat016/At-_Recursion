1c531a9ec7b13df1236f064006643b83c2a3d695
nattarat016 <123525956+nattarat016@users.noreply.github.com>
2023-01-25 10:31:05 +0700

    Update at.ts

--- at.ts
+++ at.ts
@@ -1,3 +1,15 @@
+const x = "hello"
+console.log(x.substring(1)) //ello
+console.log(x.charAt(0)) //h
+
+function reverseString(text: string) {
+    let reverseText = ""
+    for (let i = text.length - 1; i >= 0; i--) {
+        reverseText += text[i]
+    }
+    return reverseText
+}
+console.log(reverseString(x))//olleh
 
 function recursiveReverseString(text: string): string {
     if (text.length <= 0)

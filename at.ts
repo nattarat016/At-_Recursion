9e95526351b7d49804ce52c4583526b40f6262f3
nattarat016 <123525956+nattarat016@users.noreply.github.com>
2023-01-25 09:41:36 +0700

    Add files via upload

---
+++ at.ts
@@ -0,0 +1,23 @@
+// const x = "hello"
+// console.log(x.substring(1)) //ello
+// console.log(x.charAt(0)) //h
+
+// function reverseString(text: string) {
+//     let reverseText = ""
+//     for (let i = text.length - 1; i >= 0; i--) {
+//         reverseText += text[i]
+//     }
+//     return reverseText
+// }
+// console.log(reverseString(x))//olleh
+
+function recursiveReverseString(text: string): string {
+    if (text.length <= 0)
+    return text
+
+    return  recursiveReverseString(text.substring(1)) + text.charAt(0)
+}
+console.log(recursiveReverseString('hello'))//olleh
+console.log(recursiveReverseString('prayuth'))//htuyarp
+
+// console.log("hello".substring(1))
